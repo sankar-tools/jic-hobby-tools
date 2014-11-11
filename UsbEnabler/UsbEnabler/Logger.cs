154455beffb375218f09af9509407d4e8cc6d10c
sankarvema <sans.vema@gmail.com>
2014-11-11 13:06:01 +0530

    Logging added to UsbEnabler

---
+++ UsbEnabler/UsbEnabler/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsbEnabler
+{
+    class Logger
+    {
+        static System.IO.StreamWriter file = null;
+        static Logger()
+        { 
+            // initialize logger
+            Config cfg = Config.Instance();
+            string logPath = cfg.StorePath + @"\" + System.Environment.MachineName + @"\" + cfg.LogFile;
+            FileHelper.EnsurePath(logPath);
+            file = new System.IO.StreamWriter(logPath);
+        }
+
+        public static void Write(string module, string msg)
+        {
+            Console.WriteLine(String.Format("{0} :: {1}", module, msg));
+            file.WriteLine(String.Format("{0} :: {1}", module, msg));
+        }
+    }
+}
