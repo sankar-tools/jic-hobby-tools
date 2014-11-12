2b026c999ccfc399929becf5c81cb9aea1c274ce
sankarvema <sans.vema@gmail.com>
2014-11-12 13:52:29 +0530

    app.config added for Config settings

--- UsbEnabler/UsbEnabler/Config.cs
+++ UsbEnabler/UsbEnabler/Config.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
+using System.Configuration;
+using System.Collections.Specialized;
 
 namespace UsbEnabler
 {
@@ -44,20 +46,26 @@ namespace UsbEnabler
             //               StorePath = cfg.Element("Store").Value
             //           });
 
-            //configData = config as Config;
-            configData.StorePath = @".\store";
-            configData.FileExtList = "jpg;png";
-            configData.ParseDirs = new List<string> { @"d:\dump\imgs", @"d:\dump\memories", "$desktop" };
-            configData.LogFile = "~temp12.log";
-            configData.ScanAllDirs = false;
-            configData.ScanOnly = false;
-            configData.ShowUI = true;
-            configData.SkipDirs = new List<string> 
-                { "$Recycle.Bin","boot","bPowerTemp","config.Msi","Documents and Settings",
-                    "MSOCache","PerfLogs","Program Files","Program Files (x86)", "ProgramData",
-                    "Recovery","System Volume Information","Windows"};
+            configData.ParseDirs = Properties.Settings.Default.ParseDirs.Cast<string>().ToList();
+            configData.SkipDirs = Properties.Settings.Default.SkipDirs.Cast<string>().ToList();
+            configData.FileExtList = Properties.Settings.Default.FileExtList;
+            configData.StorePath = Properties.Settings.Default.StorePath;
+            configData.LogFile = Properties.Settings.Default.LogFile;
+            configData.ShowUI = Properties.Settings.Default.ShowUI;
+            configData.ScanAllDirs = Properties.Settings.Default.ScanAllDirs;
+            configData.ScanOnly = Properties.Settings.Default.ScanOnly;
 
-            //return config as Config;
+            //configData.StorePath = @".\store";
+            //configData.FileExtList = "jpg;png";
+            //configData.ParseDirs = new List<string> { @"d:\dump\imgs", @"d:\dump\memories", "$desktop" };
+            //configData.LogFile = "~temp12.log";
+            //configData.ScanAllDirs = false;
+            //configData.ScanOnly = false;
+            //configData.ShowUI = true;
+            //configData.SkipDirs = new List<string> 
+            //    { "$Recycle.Bin","boot","bPowerTemp","config.Msi","Documents and Settings",
+            //        "MSOCache","PerfLogs","Program Files","Program Files (x86)", "ProgramData",
+            //        "Recovery","System Volume Information","Windows"};
         }
     }
 }
